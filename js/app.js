81dbadc7416336ae214d024f795162f0a6b6fc3f
Mark Swearingen <markswearingen@bendcable.com>
2019-03-15 23:11:05 -0700

    Added array question, counter of correct answers, and message for exceeding the number of guesses in the number game.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var correctAnswers = 0;
+
 function getName() {
   //Get the users name to respond and provide alerts.
   var name = prompt('What is your name?'); 
   console.log('Hello ' + name);
-
+  if (name) {
+    alert ('Hello ' + name + ', Press the Start quiz button to begin');
+  }
   while(!name){
     name = prompt('Please enter your name');
   }
@@ -39,8 +43,11 @@ function collegeQuestion() {
   console.log('College States is ' + collegeStatesBoolean);
   if (collegeStatesBoolean === true){
     alert('You are correct about me going to college in both California and Oregon.  I began and graduated from Claremont McKenna College but attended the University of Oregon my sophomore year and winter term of my senior year completing my degree in March, 1985')
+    var collegeStates= '';
+    correctAnswers ++;
   }else{
     alert('I did go to college in both California and Oregon.   I began and graduated from Claremont McKenna College but attended the University of Oregon my sophomore year and winter term of my senior year completing my degree in March, 1985')
+    collegeStates = '';
   }
 
   teacherQuestion(goodAnswer);
@@ -56,7 +63,7 @@ function teacherQuestion(teacherAnswer) {
   }else {console.log('teacher else ' + teacher);
     alert ('Incorrect.  I actually did teach a driver education class at Summit High School as well as in the car training.');
   }
-
+  correctAnswers ++;
   poleQuestion(teacherAnswer);
 }
 
@@ -69,6 +76,7 @@ function poleQuestion(poleAnswer) {
     alert('Incorrect. It is a common misconception that fly fishermen use fishing poles but they actually use fishing rods.  I do use a 5 weight fly rod when fishing for trout');
   } else {console.log('pole answer is no ' + pole)
     alert('Correct. I use a 5 weight fishing rod NOT a fishing pole.');
+    correctAnswers ++;
   }
 
   flyQuestion();
@@ -84,11 +92,31 @@ function flyQuestion() {
   } else {
     if (flyType.toLowerCase() === 'yes' || flyType.toLowerCase() === 'y') {
       alert('Correct. Serendipity is a fly name');
+      correctAnswers ++;
     } else {
       if (flyType.toLowerCase() === 'no' || flyType.toLowerCase() === 'n') {
         alert('Incorrect.  Although unusual, Serendipity is a fly nane. Fly tiers have fun when naming flies.  How serendipituous is that?');
       }
     }
+  }
+
+  hobbies();
+}
+
+function hobbies(){
+  //Array with hobbies for quesion
+  var markHobbies = ['skiing', 'golf', 'fishing', 'fly fishing', 'cycling'];
+  console.log(markHobbies);
+  var hobbyGuess = prompt('What is one of my hobbies');
+  hobbyGuess = hobbyGuess.toLowerCase();
+  console.log(hobbyGuess);
+  if (hobbyGuess === 'skiing'  || hobbyGuess === 'golf'  ||hobbyGuess === 'fishing'  ||
+  hobbyGuess === 'fly fishing'  ||hobbyGuess === 'cycling') {
+    alert('Correct. '+ hobbyGuess + ' is one of my hobbies.');
+    correctAnswers ++;
+  }else {
+    console.log('else working');
+    alert('Incorrect. ' + hobbyGuess + ' is not one of my hobbies.');
   }
 
   numberQuestion();
@@ -106,6 +134,9 @@ function numberQuestion() {
   while (i < 3) {
     if (favoriteNumber === favoriteNumberGuess) { 
       alert('You are correct! My favorite number is ' + favoriteNumber);
+      correctAnswers ++;
+      alert('You got ' + correctAnswers + ' out of six questions.');
+      correctAnswers = 0;
       break;
     } else {
       i++;
@@ -118,6 +149,12 @@ function numberQuestion() {
         favoriteNumberGuess = prompt('Please guess another number higher than ' + favoriteNumberGuess);
       }
     }
+    if( i === 3){
+      alert('You ran out of guesses.  My favorite number is ' + favoriteNumber);
+      alert('You got ' + correctAnswers + ' out of six questions.');
+      correctAnswers = 0;
+    }
   }
 }
+
 
